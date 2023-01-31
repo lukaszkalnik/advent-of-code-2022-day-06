93b8f1335e0ec7f7571c11fc440d6e2832481222
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2023-01-31 21:13:03 +0100

    Solved day 6 part 1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,31 @@
+import okio.FileSystem
+import okio.Path.Companion.toPath
+import kotlin.system.measureTimeMillis
+
+fun main(args: Array<String>) {
+    val path = args[0].toPath()
+
+
+    FileSystem.SYSTEM.read(path) {
+        val message = readUtf8Line() ?: return
+
+        val runtime = measureTimeMillis {
+            var startPosition = 0
+            val markerLength = 4
+            val offset = markerLength - 1
+
+            generateSequence {
+                val chunk = message.substring(startPosition..startPosition + offset)
+                ++startPosition
+                chunk
+            }.map { chunk ->
+                val chunkSet = chunk.toSet()
+                chunkSet.size == markerLength
+            }.takeWhile { found -> !found }.toList()
+
+            val markerPosition = startPosition + offset
+            println("Marker position: $markerPosition")
+        }
+        println("Runtime: $runtime ms")
+    }
+}
